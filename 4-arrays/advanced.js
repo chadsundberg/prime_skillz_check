57315ca9c53784d558853e35c2e2ecf29f601587
Chad Sundberg <chadsundberg@gmail.com>
2017-02-13 14:18:13 -0600

    Advanced JS

--- 4-arrays/advanced.js
+++ 4-arrays/advanced.js
@@ -3,9 +3,14 @@
 // the following array.
 
 var arr = [3, 4, 6, 0];
+arr.push(32);
 
+console.log(arr);
 // 2 - Remove the first value from
 // arr using the Array built-in method slice.
-
+var sliced = arr.slice(1);
+console.log(sliced);
 
 // 3 - Add the number 11 to the beginning of arr
+arr.unshift(11);
+console.log(arr);

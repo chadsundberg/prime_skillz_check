5b0b617af425ca28b6e19a1c8d9fbde18468a1d0
Chad Sundberg <chadsundberg@gmail.com>
2017-02-13 13:40:51 -0600

    Strings

--- 1-basics/strings.js
+++ 1-basics/strings.js
@@ -1,9 +1,12 @@
 // 1 - Create a variable named phrase and assign it the value of an empty string
-
+var phrase = '';
 // 2 - Update the phrase variable by assigning it a value of "The rain in Spain falls gently on the plain"
-
+phrase = 'The rain in Spain falls gently on the plain';
 // 3 - Console log the length of phrase
-
+console.log(phrase.length);
 // 4 - Console log the word Spain by using the substring method
-
+console.log(phrase[12]+phrase[13]+phrase[14]+phrase[15]+phrase[16]);
 // 5 - Append the value ". The end." to the phrase variable.
+phrase = phrase + '. The end.';
+
+console.log(phrase);
